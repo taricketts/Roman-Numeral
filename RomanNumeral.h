c5c40ab6325072f813b53e294835812e6c23955b
Thomas Ricketts <ricketta@miamioh.edu>
2014-11-11 12:31:50 -0500

    Roman numeral project start.

---
+++ RomanNumeral.h
@@ -0,0 +1,28 @@
+/*
+ * RomanNumeral.h
+ *
+ *  Created on: Nov 10, 2014
+ *      Author: Tom
+ */
+
+#include <string>
+
+using namespace std;
+
+#ifndef ROMANNUMERAL_H_
+#define ROMANNUMERAL_H_
+
+class RomanNumeral{
+private:
+
+
+public:
+	void RomanToArabic(string input);
+
+};
+
+void RomanNumeral::RomanToArabic(string input){
+
+}
+
+#endif /* ROMANNUMERAL_H_ */
